5a18a8f891ad5f243c1f509338810b173222e858
BlaZingShooTer <kr3456525@gmail.com>
2026-07-02 12:57:31 +0530

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        int currSum = 0;
+        for(int i = 0;i<nums.size();i++){
+            
+            if(currSum<0) currSum = 0;
+
+            currSum+= nums[i];
+
+            sum = max(currSum,sum);
+        }
+        return sum;
+    }
+};
