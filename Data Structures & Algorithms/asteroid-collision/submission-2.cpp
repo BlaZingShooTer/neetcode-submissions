245f9f0706014a0c2bf30ed0b1e96efd3a21cdf3
BlaZingShooTer <kr3456525@gmail.com>
2026-08-22 06:01:26 +0530

    Add: asteroid-collision - submission-2

---
+++ Data Structures & Algorithms/asteroid-collision/submission-2.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> ans;
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            
+            int asteroid = asteroids[i];
+            bool destroyed = false;
+
+            // Check collisions
+            while (!ans.empty() && ans.top() > 0 && asteroid < 0) {
+
+                // Stack asteroid is smaller
+                if (ans.top() < -asteroid) {
+                    ans.pop();
+                }
+
+                // Both asteroids are equal
+                else if (ans.top() == -asteroid) {
+                    ans.pop();
+                    destroyed = true;
+                    break;
+                }
+
+                // Current asteroid is smaller
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            // Push asteroid if it survived
+            if (!destroyed) {
+                ans.push(asteroid);
+            }
+        }
+
+        // Convert stack to vector
+        vector<int> result;
+
+        while (!ans.empty()) {
+            result.push_back(ans.top());
+            ans.pop();
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
