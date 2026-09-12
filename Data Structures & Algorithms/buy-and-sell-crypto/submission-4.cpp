323bd555172de72acb58b55cc7942b847be9be08
BlaZingShooTer <kr3456525@gmail.com>
2026-09-12 15:13:46 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxp = 0;
+
+        int p = prices[0];
+
+        for(int i = 0;i<prices.size();i++){
+            
+           
+
+            p = min(p,prices[i]); // buy
+
+             maxp = max(maxp, prices[i] - p); //sell
+        }
+
+        return maxp;
+    }
+};
