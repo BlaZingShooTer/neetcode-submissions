3ff23e42d6a6c6a88fd0b3ac9ff97fbd8465328d
BlaZingShooTer <kr3456525@gmail.com>
2026-09-13 14:42:44 +0530

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+        stack<double> st;
+
+        vector<pair<int,int>> cars;
+
+        for(int i = 0;i<position.size();i++){  
+            cars.push_back({position[i] , speed[i]});
+        }
+
+      sort(cars.begin(), cars.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+    return a.first > b.first;
+});
+
+        for(int i = 0;i<cars.size();i++){
+            bool f = true;
+            double time = (double)(target - cars[i].first)/cars[i].second;
+
+            while(!st.empty() && st.top() >= time){
+                
+                f = false;
+            }
+            if(f){
+            st.push(time);
+
+            }
+
+        }
+
+        return st.size();
+
+    }
+};
